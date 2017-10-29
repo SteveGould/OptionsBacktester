f6373998f920b22eb5821065a87d53cf1f125163
Steve Gould <steve.gould@prolifiq.com>
2017-10-29 15:26:34 -0700

    Initial check in

---
+++ OptionsBacktester.API/Controllers/BackTestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OptionsBacktester.API.Controllers
+{
+    public class BackTestController : ApiController
+    {
+		public IHttpActionResult Index()
+		{
+
+			return null;
+		}
+	}
+}
---
+++ OptionsBacktester.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OptionsBacktester.Web.MvcApplication" Language="C#" %>
